770ce73022f9f594df35e9427d0c1aaad4caf031
Lyle Scott, III <lyle@ls3.io>
2018-08-07 00:03:39 -0400

    more refactor

--- src/gol.rs
+++ src/gol.rs
@@ -27,31 +27,8 @@ const WIDTH: usize = 55;
 const HEIGHT: usize = 55;
 
 
-#[derive(Copy, Clone)]
-pub struct Cell {
-    icon: char,
-}
-
-impl Cell {
-
-    pub fn new(icon: char) -> Self {
-        Self {
-            icon,
-        }
-    }
-
-    pub fn update(&mut self, icon: char) {
-        self.icon = icon;
-    }
-
-    pub fn is_alive(&self) -> bool {
-        self.icon == BORN_ICON || self.icon == SURVIVED_ICON
-    }
-
-}
-
 pub struct Generation {
-    state: [[Cell; WIDTH]; HEIGHT],
+    state: [[char; WIDTH]; HEIGHT],
     n: u64,
     born: u64,
     killed: u64,
@@ -62,7 +39,7 @@ impl Generation {
 
     pub fn new() -> Self {
         Self {
-            state: [[Cell::new(DEAD_ICON); WIDTH]; HEIGHT],
+            state: [[DEAD_ICON; WIDTH]; HEIGHT],
             n: 0,
             born: 0,
             killed: 0,
@@ -73,72 +50,72 @@ impl Generation {
     pub fn seed(&mut self) {
         /*
         for i in 0..HEIGHT {
-            self.state[i][0].update(BORN_ICON);
-            self.state[i][WIDTH-1].update(BORN_ICON);
+            self.state[i][0] = BORN_ICON;
+            self.state[i][WIDTH-1] = BORN_ICON;
         }
         for i in 0..WIDTH {
-            self.state[0][i].update(BORN_ICON);
-            self.state[HEIGHT-1][i].update(BORN_ICON);
-            //self.state[HEIGHT-2][i].update(KILLED_ICON);
+            self.state[0][i] = BORN_ICON;
+            self.state[HEIGHT-1][i] = BORN_ICON;
+            //self.state[HEIGHT-2][i] = KILLED_ICON;
         }
         */
 
         // THIS ONE TOO
-        self.state[0][0].update(BORN_ICON);
-        self.state[0][WIDTH-1].update(BORN_ICON);
-        self.state[HEIGHT-1][0].update(BORN_ICON);
-        self.state[HEIGHT-1][WIDTH-1].update(BORN_ICON);
-        self.state[10][10].update(BORN_ICON);
-        self.state[11][10].update(BORN_ICON);
-        self.state[12][10].update(BORN_ICON);
-        self.state[20][20].update(BORN_ICON);
-        self.state[20][21].update(BORN_ICON);
-        self.state[20][22].update(BORN_ICON);
-        self.state[17][22].update(BORN_ICON);
-        self.state[18][23].update(BORN_ICON);
-        self.state[19][24].update(BORN_ICON);
-        self.state[20][25].update(BORN_ICON);
-        self.state[17][5].update(BORN_ICON);
-        self.state[18][5].update(BORN_ICON);
-        self.state[19][5].update(BORN_ICON);
-        self.state[20][5].update(BORN_ICON);
-        self.state[17][7].update(BORN_ICON);
-        self.state[18][8].update(BORN_ICON);
-        self.state[19][9].update(BORN_ICON);
-        self.state[20][9].update(BORN_ICON);
-        self.state[20][10].update(BORN_ICON);
-        self.state[20][11].update(BORN_ICON);
-        self.state[21][11].update(BORN_ICON);
-        self.state[22][11].update(BORN_ICON);
-        self.state[23][11].update(BORN_ICON);
-        self.state[30][11].update(BORN_ICON);
-        self.state[30][12].update(BORN_ICON);
-        self.state[31][12].update(BORN_ICON);
-        self.state[31][13].update(BORN_ICON);
-        self.state[32][14].update(BORN_ICON);
-        self.state[33][15].update(BORN_ICON);
-        self.state[34][16].update(BORN_ICON);
-        self.state[35][17].update(BORN_ICON);
+        self.state[0][0] = BORN_ICON;
+        self.state[0][WIDTH-1] = BORN_ICON;
+        self.state[HEIGHT-1][0] = BORN_ICON;
+        self.state[HEIGHT-1][WIDTH-1] = BORN_ICON;
+        self.state[10][10] = BORN_ICON;
+        self.state[11][10] = BORN_ICON;
+        self.state[12][10] = BORN_ICON;
+        self.state[20][20] = BORN_ICON;
+        self.state[20][21] = BORN_ICON;
+        self.state[20][22] = BORN_ICON;
+        self.state[17][22] = BORN_ICON;
+        self.state[18][23] = BORN_ICON;
+        self.state[19][24] = BORN_ICON;
+        self.state[20][25] = BORN_ICON;
+        self.state[17][5] = BORN_ICON;
+        self.state[18][5] = BORN_ICON;
+        self.state[19][5] = BORN_ICON;
+        self.state[20][5] = BORN_ICON;
+        self.state[17][7] = BORN_ICON;
+        self.state[18][8] = BORN_ICON;
+        self.state[19][9] = BORN_ICON;
+        self.state[20][9] = BORN_ICON;
+        self.state[20][10] = BORN_ICON;
+        self.state[20][11] = BORN_ICON;
+        self.state[21][11] = BORN_ICON;
+        self.state[22][11] = BORN_ICON;
+        self.state[23][11] = BORN_ICON;
+        self.state[30][11] = BORN_ICON;
+        self.state[30][12] = BORN_ICON;
+        self.state[31][12] = BORN_ICON;
+        self.state[31][13] = BORN_ICON;
+        self.state[32][14] = BORN_ICON;
+        self.state[33][15] = BORN_ICON;
+        self.state[34][16] = BORN_ICON;
+        self.state[35][17] = BORN_ICON;
 
-        self.state[HEIGHT-1][WIDTH-1].update(BORN_ICON);
-        self.state[HEIGHT-1][WIDTH-1].update(BORN_ICON);
-        self.state[HEIGHT-1][WIDTH-1].update(BORN_ICON);
+        self.state[HEIGHT-1][WIDTH-1] = BORN_ICON;
+        self.state[HEIGHT-1][WIDTH-1] = BORN_ICON;
+        self.state[HEIGHT-1][WIDTH-1] = BORN_ICON;
 
         for i in HEIGHT-12..HEIGHT-1 {
             for j in WIDTH-6..WIDTH-1 {
-                self.state[i][j].update(BORN_ICON);
+                self.state[i][j] = BORN_ICON;
             }
         }
 
         for i in 0..8 {
             for j in 0..12 {
-                self.state[i][j].update(BORN_ICON);
+                self.state[i][j] = BORN_ICON;
             }
         }
 
         for i in 10..25 {
             for j in 30..40 {
-                self.state[i][j].update(BORN_ICON);
+                self.state[i][j] = BORN_ICON;
             }
         }
     }
@@ -151,17 +128,17 @@ impl Generation {
 
                 let n = self.neighbor_count(i, j);
 
-                if col.is_alive() {
+                if self.is_cell_alive(i, j) {
                     if n < 2 || n > 3 {
-                        f_generation.state[i][j].update(KILLED_ICON);
+                        f_generation.state[i][j] = KILLED_ICON;
                         f_generation.killed += 1;
                     } else {
-                        f_generation.state[i][j].update(SURVIVED_ICON);
+                        f_generation.state[i][j] = SURVIVED_ICON;
                         f_generation.survived += 1;
                     }
                 } else {  // is_dead, essentially :)
                     if n == 3 {
-                        f_generation.state[i][j].update(BORN_ICON);
+                        f_generation.state[i][j] = BORN_ICON;
                         f_generation.born += 1
                     }
                 }
@@ -173,40 +150,48 @@ impl Generation {
         return f_generation
     }
 
+    pub fn is_cell_alive(&self, i: usize, j: usize) -> bool {
+        match self.state[i][j] {
+            BORN_ICON => true,
+            SURVIVED_ICON => true,
+            _ => false,
+        }
+    }
+
     pub fn neighbor_count(&self, i: usize, j: usize) -> i8 {
         let l = self.state.len() - 1;
         let mut n = 0;
 
         // Above left.
-        if i > 0 && j > 0 && self.state[i-1][j-1].is_alive() {
+        if i > 0 && j > 0 && self.is_cell_alive(i-1, j-1) {
             n += 1
         }
         // Direct above.
-        if i > 0 && self.state[i-1][j].is_alive() {
+        if i > 0 && self.is_cell_alive(i-1, j) {
             n += 1
         }
         // Above right.
-        if i > 0 && j < l && self.state[i-1][j+1].is_alive() {
+        if i > 0 && j < l && self.is_cell_alive(i-1, j+1) {
             n += 1
         }
         // Direct left.
-        if j > 0 && self.state[i][j-1].is_alive() {
+        if j > 0 && self.is_cell_alive(i, j-1) {
             n += 1
         }
         // Direct right.
-        if j < l && self.state[i][j+1].is_alive() {
+        if j < l && self.is_cell_alive(i, j+1) {
             n += 1
         }
         // Below left.
-        if i < l && j > 0 && self.state[i+1][j-1].is_alive() {
+        if i < l && j > 0 && self.is_cell_alive(i+1, j-1) {
             n += 1
         }
         // Direct below.
-        if i < l && self.state[i+1][j].is_alive() {
+        if i < l && self.is_cell_alive(i+1, j) {
             n += 1
         }
         // Below right.
-        if i < l && j < l && self.state[i+1][j+1].is_alive() {
+        if i < l && j < l && self.is_cell_alive(i+1, j+1) {
             n += 1
         }
 
@@ -216,9 +201,9 @@ impl Generation {
     pub fn print(&self) {
         for row in self.state.iter() {
             for col in row.iter() {
-                match col.icon {
+                match col {
                     'b' => print!("   "),
-                    _ => print!("{} ", col.icon),
+                    _ => print!("{} ", col),
                 }
             }
             println!();
